a289b59c39a95bb507f45beeb29943d6b4dbd5e5
BearBQ <d.a.demkin@gmail.com>
2025-08-19 19:20:05 +0500

    done

--- main.go
+++ main.go
@@ -50,4 +50,79 @@ func main() {
 	//err = gorm.G[Users](db).Create(ctx, &Users{Name: "no admin", Email: "asd@asd.ru"})
 	_ = err
 
+	newPosts := []Post{
+		{Title: "1",
+			Content: "123123",
+			UserID:  1,
+		},
+		{Title: "2",
+			Content: "2222222",
+			UserID:  1},
+	}
+	for _, post := range newPosts {
+		err = AddPost(db, post)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	resultUser, err := GetUser(db, 2) //Получаем запись по номеру ID
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(resultUser)
+
+	resultUser, err = GetUserWithEmail(db, "asd@asd.ru")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(resultUser)
+
+	resultUser, err = GetUsersWithPosts(db, 1)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, post := range resultUser.Posts {
+		fmt.Println(post.Title, post.Content)
+	}
+
+	err = DeleteUserAndPosts(db, 1)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func AddPost(db *gorm.DB, post Post) error {
+
+	result := db.Create(&post)
+	return result.Error
+}
+
+func GetUser(db *gorm.DB, id uint) (Users, error) {
+	var user Users
+	result := db.First(&user, id)
+	return user, result.Error
+}
+
+func GetUserWithEmail(db *gorm.DB, email string) (Users, error) {
+	var user Users
+	result := db.Where("Email=?", email).First(&user)
+	return user, result.Error
+}
+
+func GetUsersWithPosts(db *gorm.DB, id uint) (Users, error) { //выборка пользователя с постами
+	var user Users
+	result := db.Preload("Posts").First(&user, id)
+	return user, result.Error
+}
+
+func GetAllUsersWithPosts(db *gorm.DB) (Users, error) { //выборка пользователя с постами
+	var user Users
+	result := db.Preload("Posts").Find(&user)
+	return user, result.Error
+}
+
+func DeleteUserAndPosts(db *gorm.DB, id uint) error {
+	return db.Select("Posts").Delete(&Users{}, id).Error
 }
